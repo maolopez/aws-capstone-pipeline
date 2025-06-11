7c9310d43ac034a4b4b7691437c86f72cfe45cfc
maolopez1 <mauricio.gutierrezlopez@publicissapient.com>
2025-06-11 16:25:15 -0400

    first commit

---
+++ TF_APP/main.tf
@@ -0,0 +1,12 @@
+module "my_ecr" {
+  source = "../TF_modules/ECR/"
+  ecr_repo_name = "ut_anagramma"
+  scan_on_push = false
+
+}
+
+module "my_pipeline" {
+  source = "../TF_modules/Pipeline/"
+  ecr_repo_name = "ut_anagramma"
+
+}
---
+++ TF_APP/variables.tf
@@ -0,0 +1,4 @@
+variable "ecr_repo_name" {
+  description = "The name of the repository on ECR"
+  type        = string
+}
